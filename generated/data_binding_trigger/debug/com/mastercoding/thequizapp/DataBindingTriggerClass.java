ff5808c86395fdcb863c91a55135952b9ea6c56f
Choudhary018 <124335910+Choudhary018@users.noreply.github.com>
2024-08-24 00:55:52 +0530

    Add files via upload

---
+++ generated/data_binding_trigger/debug/com/mastercoding/thequizapp/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.mastercoding.thequizapp;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
---
+++ tmp/compileDebugJavaWithJavac/compileTransaction/stash-dir/BR.java.uniqueId5
@@ -0,0 +1,5 @@
+package com.mastercoding.thequizapp;
+
+public class BR {
+  public static final int _all = 0;
+}
